4329feb005c449f4f4c6787182771fa649ac15af
Merlin Nau <merlinarthur.nau.ext@siemens-healthineers.com>
2023-09-26 17:52:40 -0500

    Initial commit

--- DWave/dimod_list_matrix/__init__.py
+++
@@ -1,6 +0,0 @@
-from dimod_list_matrix.examples import *
-import dimod_list_matrix.examples
-
-from dimod_list_matrix.data_structures import *
-from dimod_list_matrix.imports import *
-from dimod_list_matrix.create_cqm import *
--- DWave/dimod_list_matrix/examples/__init__.py
+++
@@ -1,3 +0,0 @@
-# commenting out the following line results in 
-# /usr/lib/python3.10/runpy.py:126: RuntimeWarning: 'dimod_list_matrix.examples.example' found in sys.modules after import of package 'dimod_list_matrix.examples', but prior to execution of 'dimod_list_matrix.examples.example'; this may result in unpredictable behaviour warn(RuntimeWarning(msg))
-# from dimod_list_matrix.examples.example import *
